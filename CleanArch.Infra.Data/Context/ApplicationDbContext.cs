7546d318381e6e8a4f6df474aae008071f3fc842
AndarilhoL <leonardosantos2742@gmail.com>
2022-06-14 00:42:32 -0300

    criando as configurations para as classes e criando a DbContext

---
+++ CleanArch.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //  Optando por organização do arquivo utilzando desta forma não será necessário criar
+            //  -builder.ApplyConfiguration(new CategoryConfiguration));
+            //  -builder.ApplyConfiguration(new ProductConfiguration));
+            //  o ApplyConfigurationFromAssembly vasculha os arquivos que estão na EntitiesConfiguration
+            //  que herdem de IEntityTypeConfiguration...
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
