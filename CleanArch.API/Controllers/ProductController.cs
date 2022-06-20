7195950f3341689c711989b72cebd9e2ae802bd8
AndarilhoL <leonardosantos2742@gmail.com>
2022-06-20 19:37:45 -0300

    criando o básico da controller de Product

---
+++ CleanArch.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+        {
+            var products = await _productService.GetProducts();
+            if (products is null)
+                return NotFound("Products not Found");
+
+            return Ok(products);
+        }
+    }
+}
